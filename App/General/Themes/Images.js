fd365caee5de37b69741dafd4f6f3c0903c82ed3
Ariel <arielrodrigues@protonmail.com>
2018-11-10 16:28:22 -0300

    Adiciona arquivos auxiliares para temas

---
+++ App/General/Themes/Images.js
@@ -0,0 +1,8 @@
+const images = {
+  fallbackUserPic: require('../Images/user.png'),
+  like: require('../Images/icons/like.png'),
+  liked: require('../Images/icons/liked.png'),
+  comment: require('../Images/icons/comments.png')
+}
+
+export default images
