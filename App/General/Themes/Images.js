183bac4b6406b391388d790590b9de6da19fa2ed
Ariel <arielrodrigues@protonmail.com>
2018-11-11 22:26:44 -0300

    Adiciona foto de exemplo

--- App/General/Themes/Images.js
+++ App/General/Themes/Images.js
@@ -2,7 +2,8 @@ const images = {
   fallbackUserPic: require('../Images/user.png'),
   like: require('../Images/icons/like.png'),
   liked: require('../Images/icons/liked.png'),
-  comment: require('../Images/icons/comments.png')
+  comment: require('../Images/icons/comments.png'),
+  photoExample: require('../Images/photo-example.png')
 }
 
 export default images
