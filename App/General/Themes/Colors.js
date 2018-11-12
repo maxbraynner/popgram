44dfa26dae4ce3f31c314d938a36bee2fb09902a
Ariel <arielrodrigues@protonmail.com>
2018-11-11 22:26:59 -0300

    Atualiza temas

--- App/General/Themes/Colors.js
+++ App/General/Themes/Colors.js
@@ -1,7 +1,10 @@
 const colors = {
+  background: '#FFFFFF',
+  statusBar: '#000000',
   header: '#FAFAFA',
   shaddow: 'rgba(0, 0, 0, 0.16)',
-  logoText: '#000000'
+  logoText: '#000000',
+  secondaryText: '#A8A8A8'
 }
 
 export default colors
