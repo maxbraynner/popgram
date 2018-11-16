0e57218c575193b376cd4f48a4ccefd6d13c3b9c
Ariel <arielrodrigues@protonmail.com>
2018-11-16 18:39:02 -0300

    Adiciona imagem de exemplo do usuário

--- App/General/Themes/Images.js
+++ App/General/Themes/Images.js
@@ -3,7 +3,8 @@ const images = {
   like: require('../Images/icons/like.png'),
   liked: require('../Images/icons/liked.png'),
   comment: require('../Images/icons/comments.png'),
-  photoExample: require('../Images/photo-example.png')
+  photoExample: require('../Images/photo-example.png'),
+  logoExample: require('../Images/popcodelogo.png')
 }
 
 export default images
