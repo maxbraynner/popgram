256b44c2f5248c705773681c318faaa1930c7119
Ariel <arielrodrigues@protonmail.com>
2018-11-19 17:43:34 -0300

    Adiciona tela de visualização de fotos

--- App.js
+++ App.js
@@ -7,10 +7,30 @@
  */
 
 import React, {Component} from 'react'
+import { StatusBar, View } from 'react-native'
 import TimelineScreen from './App/Timeline/Containers/TimelineScreen'
+import PostScreen from './App/Post/Containers/PostScreen'
+import { Colors, Metrics } from './App/General/Themes'
 
 export default class App extends Component<> {
   render() {
-    return (<TimelineScreen />)
+    return (
+      <View style={styles.flex}>
+        <View style={styles.statusBar}>
+          <StatusBar backgroundColor={Colors.statusBar} barStyle='light-content' />
+        </View>
+        <PostScreen />
+      </View>
+    )
+  }
+}
+
+const styles = {
+  statusBar: {
+    backgroundColor: Colors.statusBar,
+    height: Metrics.statusBarHeight
+  },
+  flex: {
+    flex: 1,
   }
 }
