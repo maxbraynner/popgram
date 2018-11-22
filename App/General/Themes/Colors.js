f9f6364f79a796d8534160524d42042d3e929d60
Ariel <arielrodrigues@protonmail.com>
2018-11-22 20:58:20 -0300

    Adiciona componente Separador

--- App/General/Themes/Colors.js
+++ App/General/Themes/Colors.js
@@ -5,7 +5,8 @@ const colors = {
   shaddow: 'rgba(0, 0, 0, 0.16)',
   logoText: '#000000',
   secondaryText: '#A8A8A8',
-  headerText: '#2E2E2E'
+  headerText: '#2E2E2E',
+  separador: 'rgba(0, 0, 0, 0.16)'
 }
 
 export default colors
---
+++ App/Login/Components/Separador.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import styles from './Styles/SeparadorStyle'
+
+class Separador extends React.Component {
+  render () {
+    return (
+      <View style={styles.separador} />
+    )
+  }
+}
+
+export default Separador
